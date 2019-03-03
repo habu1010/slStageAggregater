7779731408d1a9a17acf3a3c30e5953089c9bae4
Habu <habu1010+github@gmail.com>
2019-03-03 09:26:26 +0900

    [modify]PRP、月間獲得ファン数をツイート内容に追加

--- tweet.php
+++ tweet.php
@@ -14,19 +14,32 @@ try {
 $stmt = $pdo->query('SELECT * FROM slstage_aggregater_daily ORDER BY time DESC LIMIT 2');
 $array = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
-$today_date = date('Y/m/d H:i', $array[0]['time']);
-$yeasterday_date = date('Y/m/d H:i', $array[1]['time']);
+$dateTimeImmutable = new DateTimeImmutable();
+$today = $dateTimeImmutable->setTimestamp($array[0]['time']);
+$yesterday = $dateTimeImmutable->setTimestamp($array[1]['time']);
+$first_day_of_month = $yesterday->modify('first day of this month');
+$stmt = $pdo->prepare('SELECT * FROM slstage_aggregater_daily WHERE time >= ? ORDER BY time ASC LIMIT 1');
+$stmt->execute(array($first_day_of_month->getTimeStamp()));
+$array2 = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+$today_date = $today->format('Y/m/d H:i');
+$yeasterday_date = $yesterday->format('Y/m/d H:i');
 $level = $array[0]['level'];
 $level_increment = $array[0]['level'] - $array[1]['level'];
+$prp = $array[0]['prp'];
+$prp_increment = $array[0]['prp'] - $array[1]['prp'];
 $fan = number_format($array[0]['fan']);
 $fan_increment = number_format($array[0]['fan'] - $array[1]['fan']);
+$fan_increment_month = number_format($array[0]['fan'] - $array2[0]['fan']);
 
-$tweetStr = <<< EOF
+$tweetStr = <<<EOF
 #デレステプレイしてますけど
 $today_date (vs $yeasterday_date)
 
 レベル： $level (+ $level_increment)
+PRP： $prp (+ $prp_increment)
 ファン数： $fan (+ $fan_increment)
+月間獲得ファン数： $fan_increment_month
 詳細： {$settings['detailURL']}
 EOF;
 
